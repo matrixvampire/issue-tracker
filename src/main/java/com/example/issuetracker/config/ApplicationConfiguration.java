fbf97275ee810019a08721e25dbe889bff019cdd
Basanta Raj Onta <basanta.onta@gmail.com>
2022-03-08 07:52:11 +0000

    Add json serializer to format date for LocalDateTime

--- src/main/java/com/example/issuetracker/config/ApplicationConfiguration.java
+++ src/main/java/com/example/issuetracker/config/ApplicationConfiguration.java
@@ -1,8 +1,12 @@
 package com.example.issuetracker.config;
 
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.format.DateTimeFormatter;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,5 +16,13 @@ public class ApplicationConfiguration {
 
     @Value("${average-story-point:10}")
     private Integer averageStoryPoint;
+
+    @Value("${spring.jackson.date-format}")
+    private String dateFormat;
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateFormat)));
+    }
 
 }
